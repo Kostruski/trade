b9edf6f21ba759d1ac9caad425e67a369fa051fe
Kostruski <mkkos123@gmail.com>
2019-08-08 17:09:11 +0200

    prod

---
+++ docs/precache-manifest.f69819224916adaf02413b24a5b2ee26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b56e30ef07c088c75bc24b6fa084c4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "27b01ca70804eb906466",
+    "url": "./static/css/main.5c34d4ee.chunk.css"
+  },
+  {
+    "revision": "65cb374bfcd5e3d02ec4",
+    "url": "./static/js/2.052eb179.chunk.js"
+  },
+  {
+    "revision": "27b01ca70804eb906466",
+    "url": "./static/js/main.ff66db09.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
