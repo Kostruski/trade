d0451ba89f9e0b5ba87cbf0aade01dc4344d2626
Kostruski <mkkos123@gmail.com>
2019-08-12 16:38:01 +0200

    początek logowania

--- src/App.js
+++ src/App.js
@@ -1,131 +1,130 @@
 import React, { Component } from "react";
 import { chartsArr, wykres1 } from "./jsonFileTest.js";
-import ChartBig3 from "./components/charts/chartBig3.js"
-import ChartBig from "./components/charts/chartBig.js"
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
-
+import ChartBig3 from "./components/charts/chartBig3.js";
+import ChartBig from "./components/charts/chartBig.js";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
 import "./App.scss";
 import Header from "./components/header";
 import Section1 from "./components/section1.js";
 import Section2 from "./components/section2.js";
 import Section3 from "./components/section3.js";
 import Section4 from "./components/section4.js";
-import StartPage from "./components/startPage.js";
-import Side from "./components/side.js"
+import LoginPage from "./components/loginPage.js";
 import Contact from "./components/contact.js";
-import Chart from "./components/charts/chart.js";
-
-
 
 export default class App extends Component {
   state = {
     data1: wykres1,
     data2: chartsArr,
-    mobileSideOn: false,
-    sideVisible: true
+    isLoggedIn: false,
+    isNewUser : true,
   };
 
-  toggleMobileSideOn = () => {
-    this.setState((prev) => ({
-      mobileSideOn: !prev.mobileSideOn,
-      sideVisible: true
-
-    }))
+  
+  changeNewUser = () => this.setState({isNewUser: true})
+  toggleLogin = () => {
+    this.setState(prev => ({isLoggedIn: !prev.isLoggedIn}))
   }
-
-  toggleSide = () => {
-      this.setState((prev) => ({sideVisible: !prev.sideVisible}))
-  }
-
- 
-
 
   render() {
+    const ProtectedRoute = ({ isAllowed, ...props }) =>
+      isAllowed ? <Route {...props} /> : <Redirect to="/" />;
+
     return (
-      <div className="appWrapper">       
-      <div className="sideAndMain">
+      <div className="appWrapper">
+        <div className="sideAndMain">
+          <div className="main">
+            <Header toggleLogin={this.toggleLogin} isLoggedIn={this.state.isLoggedIn}  />
+            <Router>
+              <>
+                <Switch>
+                  <Route exact path="/" render={() => <LoginPage changeNewUser={this.changeNewUser} toggleLogin={this.toggleLogin} isLoggedIn={this.state.isLoggedIn} />} />
+                  <Route exact path="/contact" render={() => <Contact />} />
+                  <ProtectedRoute
+                    isAllowed={this.state.isLoggedIn}
+                    path="/section1"
+                    component={() => (
+                      <Section1
+                        data={this.state}
+                        handleClick={this.handleClick}
+                      />
+                    )}
+                  />
 
-      {/* <Side mobileSideOn={this.state.mobileSideOn} sideVisible={this.state.sideVisible} toggleSide={this.toggleSide}  /> */}
-         <div className="main"  >
-      <Header /> 
-      <Router>
-        <>
-          <Switch>
-            <Route exact path="/" render={() => <StartPage />} />
-            <Route exact path="/contact" render={() => <Contact />} />
-            <Route
-              path="/section1"
-              render={() => (
-                <Section1
-                  data={this.state}
-                  handleClick={this.handleClick}
-                />
-              )}
-            />
+                  <ProtectedRoute
+                    isAllowed={this.state.isLoggedIn}
+                    path="/section2"
+                    render={() => (
+                      <Section2
+                        data={this.state}
+                        handleClick={this.handleClick}
+                      />
+                    )}
+                  />
 
-            <Route
-              path="/section2"
-              render={() => (
-                <Section2
-                  data={this.state}
-                  handleClick={this.handleClick}
-                />
-              )}
-            />
+                  <ProtectedRoute
+                    isAllowed={this.state.isLoggedIn}
+                    path="/section3"
+                    render={() => (
+                      <Section3
+                        data={this.state}
+                        handleClick={this.handleClick}
+                      />
+                    )}
+                  />
 
-            <Route
-              path="/section3"
-              render={() => (
-                <Section3
-                  data={this.state}
-                  handleClick={this.handleClick}
-                />
-              )}
-            />
+                  <ProtectedRoute
+                    isAllowed={this.state.isLoggedIn}
+                    path="/section4"
+                    render={() => (
+                      <Section4
+                        data={this.state}
+                        handleClick={this.handleClick}
+                      />
+                    )}
+                  />
 
-            <Route
-              path="/section4"
-              render={() => (
-                <Section4
-                  data={this.state}
-                  handleClick={this.handleClick}
-                />
-              )}
-            />
-
-            <Route
-              path={`/${this.state.data1.id}`}
-              render={() => <ChartBig3 data={this.state.data1} />}
-            />
-            <Route
-             path={`/${this.state.data2[0].id}`}
-              render={() => <ChartBig data={this.state.data2[0]} />}
-            />
-            <Route
-               path={`/${this.state.data2[1].id}`}
-              render={() => <ChartBig data={this.state.data2[1]} />}
-            />
-            <Route
-               path={`/${this.state.data2[2].id}`}
-              render={() => <ChartBig data={this.state.data2[2]} />}
-            />
-            <Route
-              path={`/${this.state.data2[3].id}`}
-              render={() => <ChartBig data={this.state.data2[3]} />}
-            />
-            <Route
-               path={`/${this.state.data2[4].id}`}
-              render={() => <ChartBig data={this.state.data2[4]} />}
-            />
-          </Switch>
-        </>
-      </Router>
-
+                  <ProtectedRoute
+                   isAllowed={this.state.isLoggedIn}
+                    path={`/${this.state.data1.id}`}
+                    render={() => <ChartBig3 data={this.state.data1} />}
+                  />
+                  <ProtectedRoute
+                   isAllowed={this.state.isLoggedIn}
+                    path={`/${this.state.data2[0].id}`}
+                    render={() => <ChartBig data={this.state.data2[0]} />}
+                  />
+                  <ProtectedRoute
+                   isAllowed={this.state.isLoggedIn}
+                    path={`/${this.state.data2[1].id}`}
+                    render={() => <ChartBig data={this.state.data2[1]} />}
+                  />
+                  <ProtectedRoute
+                   isAllowed={this.state.isLoggedIn}
+                    path={`/${this.state.data2[2].id}`}
+                    render={() => <ChartBig data={this.state.data2[2]} />}
+                  />
+                  <ProtectedRoute
+                   isAllowed={this.state.isLoggedIn}
+                    path={`/${this.state.data2[3].id}`}
+                    render={() => <ChartBig data={this.state.data2[3]} />}
+                  />
+                  <ProtectedRoute
+                   isAllowed={this.state.isLoggedIn}
+                    path={`/${this.state.data2[4].id}`}
+                    render={() => <ChartBig data={this.state.data2[4]} />}
+                  />
+                </Switch>
+              </>
+            </Router>
+          </div>
+        </div>
       </div>
-      </div>      
-
-      </div> 
-  
     );
   }
 }
--- src/components/header.js
+++ src/components/header.js
@@ -6,8 +6,9 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import AccountCircle from "@material-ui/icons/AccountCircle";
 import Box from "@material-ui/core/Box";
-import {  HashRouter as Router, Link } from "react-router-dom";
+import { HashRouter as Router, Link } from "react-router-dom";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -15,36 +16,42 @@ const useStyles = makeStyles(theme => ({
   },
   box: {
     zIndex: 200,
+    height: 40,
     alignItems: "center",
     display: "none",
     [theme.breakpoints.up("sm")]: {
       display: "flex"
     }
   },
+
+  accountButton: {
+    justifySelf: "end"
+  },
+
   menuButton: {
-    marginRight: theme.spacing(5),
+    marginRight: theme.spacing(3),
     borderRadius: theme.shape.borderRadius
   },
   bar: {
     flexDirection: "row",
     backgroundColor: "#24344D",
-    justifyContent: "flex-start",
-    padding: "10px 30px",
-    [theme.breakpoints.up("sm")]: {
-      justifyContent: "center"
-    }
+    justifyContent: "space-between",
+    padding: "10px 30px"
+    // [theme.breakpoints.up("md")]: {
+    //   justifyContent: "center"
+    // }
   },
 
   menu: {
     "& ul": {
       backgroundColor: "#24344D",
-      color: "white",
-      
+      color: "white"
     }
   },
 
   menuIcon: {
     display: "flex",
+    flexWrap: "nowrap",
     [theme.breakpoints.up("sm")]: {
       display: "none"
     }
@@ -54,58 +61,137 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-export default function Header() {
+export default function Header(props) {
   const classes = useStyles();
   const [menuAnchor, setMenuAnchor] = React.useState(null);
+  const [profileAnchor, setProfileAnchor] = React.useState(null);
+  const isProfileOpen = Boolean(profileAnchor);
+
+  const profileClose = () => setProfileAnchor(null);
+  const profileOpen = e => setProfileAnchor(e.currentTarget);
 
   const menuClose = () => setMenuAnchor(null);
   const menuOpen = e => setMenuAnchor(e.currentTarget);
   const isMenuOpen = Boolean(menuAnchor);
 
+  const logOut = () => {
+    props.toggleLogin();
+    profileClose();
+  };
+
   return (
     <div className={classes.root}>
       <Router>
-      <AppBar position="static" className={classes.bar}>
-        <IconButton
-          edge="start"
-          className={classes.menuIcon}
-          color="inherit"
-          aria-label="menu"
-          onClick={menuOpen}
+        <AppBar position="static" className={classes.bar}>
+          <IconButton
+            edge="start"
+            className={classes.menuIcon}
+            color="inherit"
+            aria-label="menu"
+            onClick={menuOpen}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Box className={classes.box}>
+            <Button
+              className={classes.menuButton}
+              color="inherit"
+              component={Link}
+              to={"/section1"}
+            >
+              Section 1
+            </Button>
+            <Button
+              className={classes.menuButton}
+              component={Link}
+              to={"/section2"}
+              color="inherit"
+            >
+              Section 2
+            </Button>
+            <Button
+              className={classes.menuButton}
+              color="inherit"
+              component={Link}
+              to={"/section3"}
+            >
+              Section 3
+            </Button>
+            <Button
+              className={classes.menuButton}
+              color="inherit"
+              component={Link}
+              to={"/section4"}
+            >
+              Section 4
+            </Button>
+            <Button
+              className={classes.menuButton}
+              color="inherit"
+              component={Link}
+              to={"/contact"}
+            >
+              Contact
+            </Button>
+          </Box>
+          {props.isLoggedIn ? (
+            <IconButton
+              className={classes.accountButton}
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={profileOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          ) : null}
+
+          <Menu
+            id="menu-profile"
+            anchorEl={profileAnchor}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right"
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right"
+            }}
+            open={isProfileOpen}
+            onClose={profileClose}
+          >
+            <MenuItem>My Account</MenuItem>
+            <MenuItem onClick={logOut}>Log out</MenuItem>
+          </Menu>
+        </AppBar>
+
+        <Menu
+          anchorEl={menuAnchor}
+          className={classes.menu}
+          anchorOrigin={{ vertical: "top", horizontal: "right" }}
+          keepMounted
+          transformOrigin={{ vertical: "top", horizontal: "right" }}
+          open={isMenuOpen}
+          onClose={menuClose}
         >
-          <MenuIcon />
-        
-        </IconButton>
-        <Box className={classes.box}>
-          <Button className={classes.menuButton} color="inherit" component={Link} to={"/section1"}>
+          <MenuItem onClick={menuClose} component={Link} to={"/section1"}>
             Section 1
-          </Button>
-          <Button className={classes.menuButton} component={Link} to={"/section2"} color="inherit">
+          </MenuItem>
+          <MenuItem onClick={menuClose} component={Link} to={"/section2"}>
             Section 2
-          </Button>
-          <Button className={classes.menuButton} color="inherit" component={Link} to={"/section3"}>
+          </MenuItem>
+          <MenuItem onClick={menuClose} component={Link} to={"/section3"}>
             Section 3
-          </Button>
-          <Button className={classes.menuButton} color="inherit" component={Link} to={"/section4"}>
+          </MenuItem>
+          <MenuItem onClick={menuClose} component={Link} to={"/section4"}>
             Section 4
-          </Button>
-        </Box>
-      </AppBar>
-      
-      <Menu
-        anchorEl={menuAnchor}
-        className={classes.menu}
-        anchorOrigin={{ vertical: "top", horizontal: "right" }}
-        keepMounted
-        transformOrigin={{ vertical: "top", horizontal: "right" }}
-        open={isMenuOpen}
-        onClose={menuClose}
-      >
-        <MenuItem onClick={menuClose} component={Link} to={"/section1"}>Section 1</MenuItem>
-        <MenuItem onClick={menuClose} component={Link} to={"/section2"}>Section 2</MenuItem>
-        <MenuItem onClick={menuClose} component={Link} to={"/section3"}>Section 3</MenuItem>
-        <MenuItem onClick={menuClose} component={Link} to={"/section4"}>Section 4</MenuItem>
-      </Menu>
+          </MenuItem>
+          <MenuItem onClick={menuClose} component={Link} to={"/contact"}>
+            Contact
+          </MenuItem>
+        </Menu>
       </Router>
     </div>
   );
